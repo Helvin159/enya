9c27c08d0a360ac019d1d9444f30f74a231bedf5
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-20 21:58:04 -0400

    updates

--- wp-content/themes/lashtech/includes/hero.php
+++ wp-content/themes/lashtech/includes/hero.php
@@ -1,6 +1,6 @@
 <div class="container-fluid hero">
     <div class="container heroText">
         <p class="millgoudy palePink">Enya Specialize in the art of eyelash extensions</p>
-        <button class="btn button">Book a Session</button>
+        <a href="<?php echo esc_url(site_url('/shop/')) ?>" class="btn button">Book a Session</a>
     </div>
 </div>
