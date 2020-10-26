a05aae0b4af61421b6cec77e7059da49769fc3e7
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-26 14:52:59 -0400

    updates

--- wp-content/themes/lashtech/includes/banner.php
+++ wp-content/themes/lashtech/includes/banner.php
@@ -1,4 +1,4 @@
-<div class="container-fluid banner">
+<div class="container-fluid banner" style="background:url(<?php echo get_theme_file_uri('assets/img/4.jpg') ?>),linear-gradient(90deg, rgba(250,218,221, 1), rgba(250,218,221, 1));background-size:cover;background-repeat:no-repeat;background-position: left center">
     <div class="container bannerText">
         <p ><?php the_title() ?></p>
     </div>
