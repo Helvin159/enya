f9d7da032a99d54088286e92281586772d507a61
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-26 17:23:09 -0400

    updates

--- wp-content/themes/lashtech/functions.php
+++ wp-content/themes/lashtech/functions.php
@@ -14,9 +14,9 @@ function lash_files (){
   } else{
       // CSS
     //   wp_enqueue_style('my-style', get_theme_file_uri('/css/styles.css'), null, '1.0', 'all');
-      wp_enqueue_style('my-style', get_theme_file_uri('/bundled-assets/styles.9bcaff82852daa5cf3d2.css'), null, '1.0', 'all');
+      wp_enqueue_style('my-style', get_theme_file_uri('/bundled-assets/styles.23a7c471d31f123841f1.css'), null, '1.0', 'all');
       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.920bf068e75aa8ef387f.js'), NULL, '1.0', true);
-      wp_enqueue_script('main-lash-scripts', get_theme_file_uri('/bundled-assets/scripts.9bcaff82852daa5cf3d2.js'), NULL, '1.0', true);
+      wp_enqueue_script('main-lash-scripts', get_theme_file_uri('/bundled-assets/scripts.23a7c471d31f123841f1.js'), NULL, '1.0', true);
     }
 
   // Jquery
