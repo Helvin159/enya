fa2479023105fc24ce4d07f133147900586d2f36
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-27 14:57:03 -0400

    updates

--- wp-content/themes/lashtech/includes/hero.php
+++ wp-content/themes/lashtech/includes/hero.php
@@ -1,4 +1,4 @@
-<div class="container-fluid hero" style="background:url(<?php echo get_theme_file_uri('assets/img/bg2.png') ?>),linear-gradient(90deg, rgba(250,218,221, 1), rgba(250,218,221, 1));background-size:contain;background-repeat:no-repeat;background-position: center center">
+<div class="container-fluid hero" style="background:url(<?php echo get_theme_file_uri('assets/img/bg2.png') ?>),linear-gradient(90deg, rgba(250,218,221,1), rgba(250,218,221,1));">
         <!-- <p ><?php bloginfo('description')?></p> -->
         <a href="<?php echo esc_url(site_url('/contact/')) ?>" class="btn button colorD heroBtn">Book a Session</a>
 </div>
