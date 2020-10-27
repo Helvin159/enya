a630a74a040685189e0c455b4a583bcef9e733f2
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-10-27 18:16:44 -0400

    updates

--- wp-content/themes/lashtech/includes/footerdetails.php
+++ wp-content/themes/lashtech/includes/footerdetails.php
@@ -1,6 +1,6 @@
 <div class="row footerDetails">
     <div class="col-4">
-        <h1 class="px-5 colorD"><img src="<?php echo get_theme_file_uri('assets/img/logo4.png')?>" alt=""></h1>
+        <h1 class="px-5 colorD"><img src="<?php echo get_theme_file_uri('assets/img/logo6.png')?>" alt=""></h1>
         <div class="container colorD">
             <ul>
                 <li>123 Haverill St</li>
