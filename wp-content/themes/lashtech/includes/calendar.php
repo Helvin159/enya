b3b33a12bc33a75abd0d4c8834e2151953e062ee
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-10-30 21:38:35 -0400

    updates

--- wp-content/themes/lashtech/includes/calendar.php
+++ wp-content/themes/lashtech/includes/calendar.php
@@ -1,8 +1,4 @@
     <div class="container">
-
-    <?php 
-        
-        ?>
         <div class="row py-5 calendar">
             <?php 
                 // Variables
@@ -15,8 +11,6 @@
 
                 // Get days
                 $query_date = "01-$month-$year";
-                $query_date_two = "01-$month-$year";
-
                 $firstDayOfTheMonth = date('l', strtotime($query_date));
                 
                 // Weekly setup
@@ -26,44 +20,45 @@
                 while($count < $daysInMonth){ 
                     $count++; 
                     $btnId++;
-                    ?>
-                    <div  class="col-md-2 py-4 days  <?php if($count < date('j')){ echo 'previousDateOverlay';}; if($count == date('j')){ echo 'todayBg'; } ?> " >
-                    <div class="container">
-                        <h5 class="<?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?>" ><?php
-                        if($count <= 1){ 
-                            $week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-                            $firstOfTheMonthkey = array_search($firstDayOfTheMonth, $week);
-                                echo $week[$firstOfTheMonthkey];
-                                $firstOfTheMonthkey++;
-                            }
-                            else if($count >= 1){ 
-                                echo $week[$firstOfTheMonthkey];
-                                $firstOfTheMonthkey++;
-                                if($firstOfTheMonthkey == 7){
-                                    $firstOfTheMonthkey = 0;
-                                }
-                            } 
-
-                            // echo date('M-D-Y', strtotime($query_date));
-                            // echo '<br>';
-                            // First day of the month.
-                            // echo date('M-D-Y', strtotime($query_date));
-                            // echo '<br>';
-                            // echo date('D'); 
-                            // echo '<br>';         ;
-                            // Last day of the month.
-                            // echo date('m-t-Y', strtotime($query_date));
-                            ?>
-                        </h5>
-                    </div>
-                        <div id="<?php echo $count; ?>" class="container day  <?php if($count < date('j')){ echo ' ';}else{ echo 'openTime '; }; if($count == date('j')){ echo 'todayFont'; } ?>   ">
-                            <i id="<?php echo "$btnId"; ?>" class="far fa-times-circle <?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?> text-right closeBtn fontSize22"></i>
-                            <p  class="px-5 fontSize28"><?php echo $count; ?></p>  
+            ?>
+            <!-- Column for each day of the month -->
+                        <div  class="col-md-2 py-4 days 
+                            <?php 
+                                if($count < date('j')){ 
+                                    echo 'previousDateOverlay';
+                                }; 
+                                if($count == date('j')){ 
+                                    echo 'todayBg'; 
+                                } 
+                            ?>">
+                            <div class="container">
+                                <h5 class="<?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?>" >
+                                    <?php
+                                        if($count <= 1){ 
+                                            $week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+                                            $firstOfTheMonthkey = array_search($firstDayOfTheMonth, $week);
+                                            echo $week[$firstOfTheMonthkey];
+                                            $firstOfTheMonthkey++;
+                                        }
+                                        else if($count >= 1){ 
+                                            echo $week[$firstOfTheMonthkey];
+                                            $firstOfTheMonthkey++;
+                                            if($firstOfTheMonthkey == 7){
+                                            $firstOfTheMonthkey = 0;
+                                            }
+                                        } 
+                                    ?>
+                                </h5>
+                            </div>
+                            <div id="<?php echo $count; ?>" class="container day  <?php if($count < date('j')){ echo ' ';}else{ echo 'openTime '; }; if($count == date('j')){ echo 'todayFont'; } ?>   ">
+                                <i id="<?php echo "$btnId"; ?>" class="far fa-times-circle <?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?> text-right closeBtn fontSize22"></i>
+                                <p  class="px-5 fontSize28"><?php echo $count; ?></p>  
                                 <div class="agenda  text-center">
-                                    <input class="inputFields" type="text" name="name" placeholder="Anne Doe"> 
-                                    <input class="inputFields" type="tel" name="phoneNumber" placeholder="(978)888-8888">  
-                                    <input class="inputFields" type="email" name="email" placeholder="Email"> 
-                                    <input class="inputFields" type="time" name="time" >
+                                    <input id="dayInput" type="hidden" value="<?php echo $count;?>">
+                                    <input id="name" class="inputFields" type="text" name="name" placeholder="Anne Doe"> 
+                                    <input id="phone" class="inputFields" type="tel" name="phoneNumber" placeholder="(978)888-8888">  
+                                    <input id="email" class="inputFields" type="email" name="email" placeholder="Email"> 
+                                    <input id="time" class="inputFields" type="time" name="time" >
                                     
                                     <div class="container py-2">
                                         <button  href="#" type="none"  class="bookSesh btn button <?php  if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?>">
@@ -72,12 +67,8 @@
                                     </div>                                      
                                 </div>
                             </div>
-                        
-
-                    </div>
-                    
-                <?php 
-                } ?>
+                        </div>
+                <?php } ?>
                 <div class="previousDateOverlay"></div>
             </div>
         </div>
