81e13ac01c7141d3238c2809f1b4620226103ed7
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-10-30 21:02:11 -0400

    updates

--- wp-content/themes/lashtech/includes/calendar.php
+++ wp-content/themes/lashtech/includes/calendar.php
@@ -5,10 +5,7 @@
         ?>
         <div class="row py-5 calendar">
             <?php 
-
-
-
-
+                // Variables
                 $count = 0;
                 $btnId = 40;
                 $day = date('D');
@@ -26,19 +23,6 @@
                 $week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
                 $firstOfTheMonthkey = array_search($firstDayOfTheMonth, $week);
 
-                // $s = 0;
-                // while($s < $daysInMonth ){
-                //     $s++;
-                //     echo $week[$firstOfTheMonthkey +1];
-                //     if($firstOfTheMonthkey <= 7 ){
-                //         $firstOfTheMonthkey++;
-                //     } else if($firstOfTheMonthkey === 7){
-                //         $firstOfTheMonthkey = 0;
-                //         echo $week[$firstOfTheMonthkey];
-                //     }                
-                // }
-
-                
                 while($count < $daysInMonth){ 
                     $count++; 
                     $btnId++;
@@ -59,9 +43,7 @@
                                     $firstOfTheMonthkey = 0;
                                 }
                             } 
-                            ?>
-                        </h5>
-                            <?php 
+
                             // echo date('M-D-Y', strtotime($query_date));
                             // echo '<br>';
                             // First day of the month.
@@ -72,15 +54,14 @@
                             // Last day of the month.
                             // echo date('m-t-Y', strtotime($query_date));
                             ?>
-
-
+                        </h5>
                     </div>
                         <div id="<?php echo $count; ?>" class="container day  <?php if($count < date('j')){ echo ' ';}else{ echo 'openTime '; }; if($count == date('j')){ echo 'todayFont'; } ?>   ">
                             <i id="<?php echo "$btnId"; ?>" class="far fa-times-circle <?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?> text-right closeBtn fontSize22"></i>
                             <p  class="px-5 fontSize28"><?php echo $count; ?></p>  
                                 <div class="agenda  text-center">
                                     <input class="inputFields" type="text" name="name" placeholder="Anne Doe"> 
-                                    <input class="inputFields" type="tel" name="phoneNumber" placeholder="(978)888-888">  
+                                    <input class="inputFields" type="tel" name="phoneNumber" placeholder="(978)888-8888">  
                                     <input class="inputFields" type="email" name="email" placeholder="Email"> 
                                     <input class="inputFields" type="time" name="time" >
                                     
@@ -97,9 +78,7 @@
                     
                 <?php 
                 } ?>
-                <div class="previousDateOverlay">
-                    
-                </div>
+                <div class="previousDateOverlay"></div>
             </div>
         </div>
         <div class="container py-5 hideContent thankYou text-center">
