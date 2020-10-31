c210f0570a7a0f923b6972f9eb0c644ad3e9a5b3
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-10-30 20:00:33 -0400

    updates

--- wp-content/themes/lashtech/includes/calendar.php
+++ wp-content/themes/lashtech/includes/calendar.php
@@ -1,20 +1,78 @@
     <div class="container">
+
+    <?php 
+        
+        ?>
         <div class="row py-5 calendar">
             <?php 
+
+
+
+
                 $count = 0;
                 $btnId = 40;
                 $day = date('D');
                 $month = date('m');
                 $year = date('Y');
-                // $timeIntervals = 45;
-                $hours = 24;
                 $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-    
+
+                // Get days
+                $query_date = "01-$month-$year";
+                $query_date_two = "01-$month-$year";
+
+                $firstDayOfTheMonth = date('l', strtotime($query_date));
+                
+                // Weekly setup
+                $week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+                $firstOfTheMonthkey = array_search($firstDayOfTheMonth, $week);
+
+                // $s = 0;
+                // while($s < $daysInMonth ){
+                //     $s++;
+                //     echo $week[$firstOfTheMonthkey +1];
+                //     if($firstOfTheMonthkey <= 7 ){
+                //         $firstOfTheMonthkey++;
+                //     } else if($firstOfTheMonthkey === 7){
+                //         $firstOfTheMonthkey = 0;
+                //         echo $week[$firstOfTheMonthkey];
+                //     }                
+                // }
+
+                
                 while($count < $daysInMonth){ 
                     $count++; 
                     $btnId++;
                     ?>
                     <div  class="col-md-2 py-4 days  <?php if($count < date('j')){ echo 'previousDateOverlay';}; if($count == date('j')){ echo 'todayBg'; } ?> " >
+                    <div class="container">
+                        <h5 class="<?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?>" ><?php
+                        if($count <= 1){ 
+                            $week = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+                            $firstOfTheMonthkey = array_search($firstDayOfTheMonth, $week);
+                                echo $firstDayOfTheMonth;}
+                            else if($count > 1){ 
+                                echo $week[$firstOfTheMonthkey];
+                                $firstOfTheMonthkey++;
+                                if($firstOfTheMonthkey == 7){
+                                    $firstOfTheMonthkey = 0;
+                                }
+                            } 
+                            ?>
+                        </h5>
+                            <?php 
+                            // echo date('M-D-Y', strtotime($query_date));
+                            // echo '<br>';
+                            // First day of the month.
+                            // echo date('M-D-Y', strtotime($query_date));
+                            // echo '<br>';
+                            // echo date('D'); 
+                            // echo '<br>';         ;
+                            // Last day of the month.
+                            // echo date('m-t-Y', strtotime($query_date));
+                            ?>
+
+
+                    </div>
                         <div id="<?php echo $count; ?>" class="container day  <?php if($count <= date('j')){ echo ' ';}else{ echo 'openTime'; }; if($count == date('j')){ echo 'todayFont'; } ?>   ">
                             <i id="<?php echo "$btnId"; ?>" class="far fa-times-circle <?php if($count == date('j')){ echo 'todayFont'; }else{ echo 'colorD';} ?> text-right closeBtn fontSize22"></i>
                             <p  class="px-5 fontSize28"><?php echo $count; ?></p>  
@@ -34,6 +92,7 @@
                         
 
                     </div>
+                    
                 <?php 
                 } ?>
                 <div class="previousDateOverlay">
