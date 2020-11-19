917a9c08e3bb66c39d4c17b65fec5185ed78e885
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-19 17:32:04 -0500

    updates calendly

--- wp-content/themes/lashtech/includes/calendly.php
+++ wp-content/themes/lashtech/includes/calendly.php
@@ -1,4 +1,4 @@
 <!-- Calendly inline widget begin -->
-<div class="calendly-inline-widget" data-url="https://calendly.com/enyacalderon/90min?text_color=3a2c2e&primary_color=c7a0a6" style="min-width:320px;height:900px;"></div>
-<script type="text/javascript" src="<?php echo get_theme_file_uri('js/modules/calendly.js')?>"></script>
+<div class="calendly-inline-widget" data-url="https://calendly.com/enyacalderon/90min?text_color=3a2c2e&primary_color=c7a0a6" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
 <!-- Calendly inline widget end -->
