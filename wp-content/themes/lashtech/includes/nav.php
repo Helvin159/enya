8fe1b9ab821f66d160ce03873b4138ed6feae8dd
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-20 16:14:27 -0500

    updates

--- wp-content/themes/lashtech/includes/nav.php
+++ wp-content/themes/lashtech/includes/nav.php
@@ -7,9 +7,9 @@
   <div class="collapse navbar-collapse m-auto" id="navbarSupportedContent">
     <ul class="navbar-nav">
       <?php
-      if($_SERVER['REQUEST_URI'] != '/' or $_SERVER['REQUEST_URI'] != '/pages'){?>
+      if($_SERVER['REQUEST_URI'] != '/' or $_SERVER['REQUEST_URI'] != '/pages/enya/'){?>
         <li class="nav-item  ">
-        <a class="nav-link colorD fontSize20 <?php if($_SERVER['REQUEST_URI'] == '/'){echo 'active';}; ?>" href="<?php echo esc_url(site_url('/'))?>">Home <?php echo $_SERVER['REQUEST_URI'];?></a>
+        <a class="nav-link colorD fontSize20 <?php if($_SERVER['REQUEST_URI'] == '/'){echo 'active';}; ?>" href="<?php echo esc_url(site_url('/'))?>">Home </a>
       </li>
       <?php } ?>
       <li class="nav-item  ">
